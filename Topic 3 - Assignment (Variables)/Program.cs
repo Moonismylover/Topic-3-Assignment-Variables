18b16bb0045dc19fb29d69c89214a88f0bec1506
700014473 <700014473@2260I-L29-008>
2026-02-10 13:13:35 -0500

    ytjt

--- Topic 3 - Assignment (Variables)/Program.cs
+++ Topic 3 - Assignment (Variables)/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Console.Title = "Topic 3 - Assignment (Variables)";
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
         }
     }
 }

5914bd1e83c3ab6be2f06fbf5e736063334c7e53
700014473 <700014473@2260I-L29-008>
2026-02-10 13:18:57 -0500

    yhjty

--- Topic 3 - Assignment (Variables)/Program.cs
+++ Topic 3 - Assignment (Variables)/Program.cs
@@ -4,9 +4,20 @@
     {
         static void Main(string[] args)
         {
+            // Angel 
+
             Console.Title = "Topic 3 - Assignment (Variables)";
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("What is your name?");
+
+            int roomNumber = 29;
+            double price = 2.99;
+            string courseName = "Computer Science";
+
+            Console.WriteLine("This is room " + roomNumber);
+            Console.WriteLine(" ");
+            Console.WriteLine("The price is " + price.ToString("C"));
+            Console.WriteLine("");
+            Console.WriteLine("I am learning a bit about " + courseName);
+
         }
     }
 }
